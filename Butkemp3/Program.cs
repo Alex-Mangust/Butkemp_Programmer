837141dd4d68764715868df0483f7db97ac9f157
Alex Mangust <sanya.stogny@gmail.com>
2023-09-19 00:10:11 +0800

    Разобрали от чего зависит сложность алгоритма

--- Butkemp3/Program.cs
+++ Butkemp3/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int n = 5;
+int [] array = new int[n];
+for(int i = 0; i < 5; i++)
+    array[i] = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("[" + string.Join(" ", array) + "]");
+// [4, 5, 3, 1, 2]
+// O(n)
+// [1, 2, 3, 4, 5] - O(n * log n)
+// ((5+1)/2) * 5 - O(1)
+// n < n * log(n) + 1
+int summa = 0;
+for(int i = 0; i < n; i++)
+    summa += array[i];
+System.Console.WriteLine(summa);
+// O(n)
