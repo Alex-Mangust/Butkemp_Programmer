673a7fa8d50098055f3620e6e062e642a56b6bac
Alex Mangust <sanya.stogny@gmail.com>
2023-09-20 00:23:55 +0800

    Закончили работу с 6 буткемпом

--- Butkemp6/Program.cs
+++ Butkemp6/Program.cs
@@ -38,10 +38,28 @@
 // }
 // System.Console.WriteLine($"[{string.Join(", ", array)}]");
 
-
+using System.Diagnostics;
 Console.Clear();
+bool show = true;
 System.Console.Write("Введите кол-во элементов: ");
 int n = int.Parse(Console.ReadLine()!);
+bool inputShow = false;
+while(!inputShow)
+{
+    System.Console.WriteLine("Вывести массисы в консоль? \n 1 - да \t 2 - нет");
+    int shows = int.Parse(Console.ReadLine()!);
+    if(shows == 1)
+    {
+        show = true;
+        inputShow = true;
+    }
+    else if(shows == 2)
+    {
+        show = false;
+        inputShow = true;
+    }
+    else System.Console.WriteLine("Пожалуйста, введите либо 1, либо 2");
+}
 int[] array = new int[n];
 
 bool Check(int[] array)  // Пример юнит-теста (Проверяет, отсортирован ли массив)
@@ -56,22 +74,47 @@ bool Check(int[] array)  // Пример юнит-теста (Проверяет
 
 for(int i = 0; i < array.Length; i++)
     array[i] = Random.Shared.Next(100);
-System.Console.WriteLine($"[{string.Join(", ", array)}]");
-System.Console.WriteLine(Check(array));
-for(int k = 0; k < array.Length - 1; k++)
+int[] arr1 = new int[n];
+int[] arr2 = new int[n];
+Array.Copy(array, arr1, n);
+Array.Copy(array, arr2, n);
+if(show) System.Console.WriteLine($"[{string.Join(", ", arr1)}]");
+Stopwatch sw = new Stopwatch();
+sw.Start();
+for(int k = 0; k < arr1.Length - 1; k++)
 {
-    for(int i = 0; i < array.Length - 1 - k; i++)
+    for(int i = 0; i < arr1.Length - 1 - k; i++)
     {
-        if(array[i] > array[i+1])
+        if(arr1[i] > arr1[i+1])
         {
-            int temp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = temp;
+            int temp = arr1[i];
+            arr1[i] = arr1[i + 1];
+            arr1[i + 1] = temp;
         }
     }
 }
-System.Console.WriteLine($"[{string.Join(", ", array)}]");
-System.Console.WriteLine(Check(array));
+sw.Stop();
+System.Console.WriteLine($"arr1 - {Check(arr1)} {sw.ElapsedMilliseconds}ms");
+if(show) System.Console.WriteLine($"arr1: [{string.Join(", ", arr1)}]");
+Console.ReadLine();
+if(show) System.Console.WriteLine($"[{string.Join(", ", arr2)}]");
+sw.Reset();
+sw.Start();
+for(int k = 0; k < arr2.Length - 1; k++)
+{
+    for(int i = 0; i < arr2.Length - 1; i++)
+    {
+        if(arr2[i] > arr2[i+1])
+        {
+            int temp = arr2[i];
+            arr2[i] = arr2[i + 1];
+            arr2[i + 1] = temp;
+        }
+    }
+}
+sw.Stop();
+System.Console.WriteLine($"arr2 - {Check(arr2)} {sw.ElapsedMilliseconds}ms");
+if(show) System.Console.WriteLine($"arr2: [{string.Join(", ", arr2)}]");
 
 // int j = 0;
 // for(j = 0; j < n; j++)
