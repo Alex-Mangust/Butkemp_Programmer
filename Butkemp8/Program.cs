dfdf0e1087049a76780bdb5607456404bd5257c9
Alex Mangust <sanya.stogny@gmail.com>
2023-09-22 16:33:48 +0800

    Написали небольшой код, показывающий как работает рекурсия

--- Butkemp8/Program.cs
+++ Butkemp8/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Быстрая Сортировка O(n * log2(n))
+// Напишите программу, которая сложит 2 числа a и b, без прямого сложения
+
+int sumNumbers(int x, int y)
+{
+    if(x == 0)
+        return y;
+    return sumNumbers(x - 1, y + 1);
+}
+
+Console.Clear();
+System.Console.Write("Введите 2 числа: ");
+int[] numbers = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+System.Console.WriteLine
+($"Результат {numbers[0]} + {numbers[1]} = {sumNumbers(numbers[0], numbers[1])}");
