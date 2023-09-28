163bf7fdbef387a46d9eccdfdbf9d0080441bae6
Alex Mangust <sanya.stogny@gmail.com>
2023-09-28 20:19:11 +0800

    Написали код, делающий сортировку подсчетом массива с элементами от 0 до максимального элемента

--- Butkemp13/Program.cs
+++ Butkemp13/Program.cs
@@ -1,34 +1,29 @@
 ﻿// Сортировка подсчетом
 
 using System.Globalization;
-
-Console.Clear();
-int[] array = new int[] { 0, 2, 3, 2, 31, 5, 9, 1, 1, 0, 3, 4, 20 };
-int max = int.MinValue;
-for (int i = 0; i < array.Length; i++)
-    if (max < array[i])
-        max = array[i];
-void CountingSort(int[] inputArray)
+int[] CountingSortExtended(int[] inputArray)
 {
+    // int max = int.MinValue;
+    // for (int i = 0; i < inputArray.Length; i++)
+    // if (max < inputArray[i])
+    //     max = inputArray[i];
+    int max = inputArray.Max();
+    int[] sortedArray = new  int[inputArray.Length];
     int[] counters = new int[max + 1];
-    // int ourNumber;
-    for (int i = 0; i < inputArray.Length; i++) // массив повторений
-    {
+    for(int i = 0; i < inputArray.Length; i++)
         counters[inputArray[i]]++;
-        // ourNumber = inputArray[i];
-        // counters[ourNumber]++;
-    }
-
     int index = 0;
-    for (int i = 0; i < counters.Length; i++)
-    {
-        for (int j = 0; j < counters[i]; j++)
+    for(int i = 0; i < counters.Length; i++)
+        for(int j = 0; j < counters[i]; j++)
         {
-            inputArray[index] = i;
+            sortedArray[index] = i;
             index++;
         }
-    }
+    return sortedArray;
 }
+
+Console.Clear();
+int[] array = new int[] {0, 2, 4, 10, 20, 5, 6, 1, 2};
+int[] sortedArray = CountingSortExtended(array);
 System.Console.WriteLine(string.Join(", ", array));
-CountingSort(array);
-System.Console.WriteLine(string.Join(", ", array));
+System.Console.WriteLine(string.Join(", ", sortedArray));
