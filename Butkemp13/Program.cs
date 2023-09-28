50c1a3a48461b245679b07f3cc114a6781ceb1c6
Alex Mangust <sanya.stogny@gmail.com>
2023-09-28 19:10:01 +0800

    Начали работу с 13 буткемпом

---
+++ Butkemp13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Сортировка подсчетом
+
+int[] array = new int[] {0,2,3,2,1,5,9,1,1};
+
+void CountingSort(int[] inputArray)
+{
+    int[]counters = new int[10];
+    // int ourNumber;
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+        counters[inputArray[i]]++;
+        // ourNumber = inputArray[i];
+        // counters[ourNumber]++;
+    }
+
+    for(int i = 0; i < inputArray.Length; i++)
+    {
+
+    }
+}
