bb3bf0b88bacc71a91b599fdffe8999753fdea05
Alex Mangust <sanya.stogny@gmail.com>
2023-09-28 20:40:04 +0800

    Написали код, делающий сортировку подсчетом массива с элементами от минимального до максимального элемента

--- Butkemp13/Program.cs
+++ Butkemp13/Program.cs
@@ -8,22 +8,25 @@ int[] CountingSortExtended(int[] inputArray)
     // if (max < inputArray[i])
     //     max = inputArray[i];
     int max = inputArray.Max();
+    int min = inputArray.Min();
+    int offset = -min;
     int[] sortedArray = new  int[inputArray.Length];
-    int[] counters = new int[max + 1];
+    int[] counters = new int[max + offset + 1];
+    System.Console.WriteLine(max + offset + 1);
     for(int i = 0; i < inputArray.Length; i++)
-        counters[inputArray[i]]++;
+        counters[inputArray[i] + offset]++;
     int index = 0;
     for(int i = 0; i < counters.Length; i++)
         for(int j = 0; j < counters[i]; j++)
         {
-            sortedArray[index] = i;
+            sortedArray[index] = i - offset;
             index++;
         }
     return sortedArray;
 }
 
 Console.Clear();
-int[] array = new int[] {0, 2, 4, 10, 20, 5, 6, 1, 2};
+int[] array = new int[] {-12, 32, -22, 10, 21, -4};
 int[] sortedArray = CountingSortExtended(array);
 System.Console.WriteLine(string.Join(", ", array));
 System.Console.WriteLine(string.Join(", ", sortedArray));
