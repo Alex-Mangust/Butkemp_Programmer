113fd133a3665f26a401e41789dd315fc3429ea3
Alex Mangust <sanya.stogny@gmail.com>
2023-09-28 19:53:04 +0800

    Написали код, делающий сортировку подсчетом массива с элементами от 0 до 10

--- Butkemp13/Program.cs
+++ Butkemp13/Program.cs
@@ -1,20 +1,34 @@
 ﻿// Сортировка подсчетом
 
-int[] array = new int[] {0,2,3,2,1,5,9,1,1};
+using System.Globalization;
 
+Console.Clear();
+int[] array = new int[] { 0, 2, 3, 2, 31, 5, 9, 1, 1, 0, 3, 4, 20 };
+int max = int.MinValue;
+for (int i = 0; i < array.Length; i++)
+    if (max < array[i])
+        max = array[i];
 void CountingSort(int[] inputArray)
 {
-    int[]counters = new int[10];
+    int[] counters = new int[max + 1];
     // int ourNumber;
-    for(int i = 0; i < inputArray.Length; i++)
+    for (int i = 0; i < inputArray.Length; i++) // массив повторений
     {
         counters[inputArray[i]]++;
         // ourNumber = inputArray[i];
         // counters[ourNumber]++;
     }
 
-    for(int i = 0; i < inputArray.Length; i++)
+    int index = 0;
+    for (int i = 0; i < counters.Length; i++)
     {
-
+        for (int j = 0; j < counters[i]; j++)
+        {
+            inputArray[index] = i;
+            index++;
+        }
     }
 }
+System.Console.WriteLine(string.Join(", ", array));
+CountingSort(array);
+System.Console.WriteLine(string.Join(", ", array));
