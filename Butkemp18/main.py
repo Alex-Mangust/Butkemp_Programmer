4ff8f50cda02db2358773feb565244cd43ee67c4
Alex Mangust <sanya.stogny@gmail.com>
2023-10-06 19:45:31 +0800

    Закончили работу с 18 буткемпом

--- Butkemp18/main.py
+++ Butkemp18/main.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask import render_template
+from random import randint
+
 app = Flask(__name__)
+
+
 @app.route('/')
 def say_hello():
-    return render_template('index.html', name='Том')
+    context = {
+        "title": "Тестовая страница",
+        "name": "Алекс",
+        'number': randint(1, 6),
+        'temp_list': ["Bob", "Ann", "Asya", "Alex", "Eva"]
+    }
+    return render_template('index.html', **context)
+
 
 @app.route('/test/')
 def say_start():
     return render_template('name.html')
-if __name__ == '__name__':
+
+
+if __name__ == '__main__':
     app.run(debug=True)
